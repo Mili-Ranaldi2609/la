2158b253eb2540f430d9492018d3a67e2608d43e
Mili Ranaldi <mranaldi@gmail.com>
2023-11-07 18:42:44 -0300

    parcial terminado

---
+++ parcial2.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+public class parcial2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("////Menú////");
+            System.out.println("(1) Detectar si un ADN es mutante");
+            System.out.println("(2) Salir");
+            System.out.print(": ");
+
+            String option = scanner.nextLine();
+
+            if (option.equals("1")) {
+                char[][] adn = new char[6][6];
+                for (int i = 0; i < 6; i++) {
+                    while (true) {
+                        System.out.print("Ingrese la fila " + (i + 1) + " de la secuencia de ADN (exactamente 6 letras): ");
+                        String row = scanner.nextLine().toUpperCase();
+                        // Verifica que la fila tenga 6 letras válidas (ATCG):
+                        if (row.length() == 6 && row.matches("[ATCG]+")) {
+                            adn[i] = row.toCharArray();
+                            break;
+                        } else {
+                            System.out.println("Error: Debe ingresar exactamente 6 letras válidas (ATCG).");
+                        }
+                    }
+                }
+
+                if (esMutante(adn)) {
+                    System.out.println("El humano es mutante (ºOº). Sera reclutado por MAGNETO.");
+                } else {
+                    System.out.println("El humano no es mutante (-_-). Prueba finalizada.");
+                }
+            } else if (option.equals("2")) {
+                System.out.println("Programa finalizado (>u<)");
+                break;
+            } else {
+                System.out.println("Opción inválida. Por favor, elija nuevamente.");
+            }
+        }
+    }
+
+    public static boolean esMutante(char[][] adn) {
+        int filas = adn.length;
+        int columnas = adn[0].length;
+
+        // Definir los patrones a buscar para secuencias mutantes
+        String[] patrones = {"AAAA", "TTTT", "CCCC", "GGGG"};
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                // Verificar los patrones en todas las direcciones
+                for (String patron : patrones) {
+                    // Horizontal
+                    if (j + patron.length() <= columnas) {
+                        String secuencia = new String(adn[i], j, patron.length());
+                        if (secuencia.equals(patron)) {
+                            return true;
+                        }
+                    }
+
+                    // Vertical
+                    if (i + patron.length() <= filas) {
+                        StringBuilder secuencia = new StringBuilder();
+                        for (int k = 0; k < patron.length(); k++) {
+                            secuencia.append(adn[i + k][j]);
+                        }
+                        if (secuencia.toString().equals(patron)) {
+                            return true;
+                        }
+                    }
+
+                    // Diagonal de izquierda a derecha
+                    if (i + patron.length() <= filas && j + patron.length() <= columnas) {
+                        StringBuilder secuencia = new StringBuilder();
+                        for (int k = 0; k < patron.length(); k++) {
+                            secuencia.append(adn[i + k][j + k]);
+                        }
+                        if (secuencia.toString().equals(patron)) {
+                            return true;
+                        }
+                    }
+
+                    // Diagonal de derecha a izquierda
+                    if (i + patron.length() <= filas && j - patron.length() + 1 >= 0) {
+                        StringBuilder secuencia = new StringBuilder();
+                        for (int k = 0; k < patron.length(); k++) {
+                            secuencia.append(adn[i + k][j - k]);
+                        }
+                        if (secuencia.toString().equals(patron)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
